f47593e444133d327121ef1580ff055ef6275edd
Benjamin M <bmalmh@gmail.com>
2024-05-06 09:19:38 +0200

    scene detect initial typing

---
+++ packages/scenedetect/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+	env: {
+		node: true,
+		es6: true
+	},
+	extends: 'eslint:recommended',
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always']
+	}
+}
