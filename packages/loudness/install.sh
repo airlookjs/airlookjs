a86449506a3cd91908861c8d159e2e774d47f1ff
Johan Bichel <public@johan.cc>
2024-05-10 08:54:14 +0200

    fix(loudness): compiles on mac os, disabled unneccesarry gui deps

--- packages/loudness/install.sh
+++ packages/loudness/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf loudness-scanner
-git clone --recurse-submodules -j8 https://github.com/airlookjs/loudness-scanner
+git clone --recurse-submodules -j8 --depth=1 https://github.com/airlookjs/loudness-scanner
 cd loudness-scanner
 
 if [ "$(uname)" == "Darwin" ]; then
@@ -9,24 +9,20 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install glib  
   brew install gstreamer  
   brew install taglib   
-  brew install mpg123  
+  #brew install mpg123  
   #brew install librsvg  
-  brew install qt@5  
+  #brew install qt@5  
   brew install libsndfile  
   #brew install gtk 
-
-  export Qt5_DIR="/opt/homebrew/opt/qt@5/lib/cmake/Qt5"
+  #export Qt5_DIR="/opt/homebrew/opt/qt@5/lib/cmake/Qt5"
   export PATH="/opt/homebrew/opt/ffmpeg@4/bin:$PATH"
   export PKG_CONFIG_PATH="/opt/homebrew/opt/ffmpeg@4/lib/pkgconfig"
-  #:/opt/homebrew/opt/librsvg/lib/pkgconfig"
   export CXX=g++
   export CC=gcc
-  #export CFLAGS="pkg-config --cflags --libs librsvg-2.0"
-  #export LDFLAGS"-lobjc"
-  #export LDFLAGS="pkg-config --libs librsvg-2.0 -L/opt/homebrew/opt/ffmpeg@4/lib"
 fi
+
 
 mkdir build
 cd build
-cmake .. -DDISABLE_RSVG2:BOOL=yes -DDISABLE_GTK2:BOOL=yes
-make
+cmake .. -DDISABLE_RSVG2=yes -DDISABLE_GTK2=yes -DDISABLE_QT5=yes -DENABLE_INTERNAL_QUEUE_H=ON
+make 
