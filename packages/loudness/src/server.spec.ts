699d98a94ea3fb4cd02a27225c475a2351644d5d
Johan Bichel <public@johan.cc>
2024-05-14 00:23:07 +0200

    compare the data

--- packages/loudness/src/server.spec.ts
+++ packages/loudness/src/server.spec.ts
@@ -2,7 +2,6 @@ import { server } from './server.js';
 import request from "supertest";
 import { expect, describe, it, vi } from "vitest";
 
-
 // TODO: share matches and cached
 // mock configuration
 vi.mock('../src/config.ts', async(importOriginal) => {
@@ -36,18 +35,19 @@ describe('loudness', () => {
 
     it('should return 400 Bad Request with no query params', async () => {
         const res = await request(server).get('/api/loudness');
-        console.log(res.body);
         expect(res.status).toBe(400);
     });
 
-    it('should return valid result for loudness', async (t) => {
+    it('should return valid result for loudness', { timeout: 10000 }, async () => {
 
         // Test file from https://tech.ebu.ch/publications/ebu_loudness_test_set
         const AUDIO_TEST_FILE = 'seq-3341-13-1-24bit.wav'; 
         const res = await request(server).get(`/api/loudness?file=tests/${AUDIO_TEST_FILE}`);
-        expect(res.status).toBe(200);
 
-    }, { timeout: 10000 });
+        expect(res.status).toBe(200);
+        expect(res.body).toEqual({"loudness":{"sampleRate":0.02,"lra":0,"lufs":-25,"integratedValues":[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,-23,-23,-23,-23,-23,-23.6,-23.6,-23.6,-23.6,-23.6,-24.2,-24.2,-24.2,-24.2,-24.2,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25,-25],"momentaryValues":[-36,-33,-31.2,-30,-29,-28.2,-27.6,-27,-26.5,-26,-25.6,-25.2,-24.9,-24.5,-24.2,-24,-23.7,-23.5,-23.2,-23,-23.2,-23.5,-23.7,-24,-24.2,-24.5,-24.9,-25.2,-25.6,-26,-26.5,-27,-27.6,-28.2,-29,-30,-31.2,-33,-36,-65.7,-89.8,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90,-90],"shorttermValues":[-44.8,-41.7,-40,-38.7,-37.8,-37,-36.3,-35.7,-35.2,-34.8,-34.3,-34,-33.6,-33.3,-33,-32.7,-32.5,-32.2,-32,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7,-31.7]}});
+
+    });
 
 
     // TODO: test caching on shares
