2db7fbc401f6a33dc26eac1bcd7a7f5dcc478790
Johan Bichel <public@johan.cc>
2024-05-13 14:08:51 +0200

    ts lint fixes

--- packages/mediainfo/src/config.ts
+++ packages/mediainfo/src/config.ts
@@ -49,7 +49,7 @@ export const config : {
 } = {
 	version: process.env.npm_package_version ?? 'dev',
 	port: parseIntEnv(process.env.PORT, 3000),
-	route: process.env.ROUTE_PREFIX || '/api/mediainfo',
-	defaultOutputFormatName: process.env.DEFAULT_OUTPUT_FORMAT || 'EBUCore_JSON',
+	route: process.env.ROUTE_PREFIX ?? '/api/mediainfo',
+	defaultOutputFormatName: process.env.DEFAULT_OUTPUT_FORMAT ?? 'EBUCore_JSON',
 	shares: []
 }
