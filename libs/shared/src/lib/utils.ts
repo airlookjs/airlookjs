18080bf732055e5edcbbe1b5eee2892660850c33
Johan Bichel <public@johan.cc>
2024-05-14 23:17:49 +0200

    move env parsingt to lib shared

--- libs/shared/src/lib/utils.ts
+++ libs/shared/src/lib/utils.ts
@@ -7,3 +7,29 @@ export interface ShareInfo {
 	systemRoot: string;
 	matches: RegExp[];
 }
+
+// parse ints from env safely
+export const parseIntEnv = (env:  string | undefined, defaultValue: number) : number => {
+	if (env === undefined) {
+		return defaultValue;
+	}
+	const parsed = parseInt(env);
+	if (isNaN(parsed)) {
+		return defaultValue;
+	}
+	return parsed;
+};
+
+// parse bools from env safely
+export const parseBoolEnv = (env: string | undefined, defaultValue: boolean) : boolean => {
+	if (env === undefined) {
+		return defaultValue;
+	}
+	if (env === 'true') {
+		return true;
+	}
+	if (env === 'false') {
+		return false;
+	}
+	return defaultValue;
+};
--- packages/scenedetect/src/config.ts
+++ packages/scenedetect/src/config.ts
@@ -1,4 +1,4 @@
-import { ShareInfo } from '@airlookjs/shared';
+import { parseIntEnv, type ShareInfo } from '@airlookjs/shared';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,32 +10,6 @@ export interface SceneDetectConfig {
 	version: string;
 	port: number;
 }
-
-// parse bools from env safely
-/*const parseBoolEnv = (env: string | undefined, defaultValue: boolean) => {
-	if (env === undefined) {
-		return defaultValue;
-	}
-	if (env === 'true') {
-		return true;
-	}
-	if (env === 'false') {
-		return false;
-	}
-	return defaultValue;
-};*/
-
-// parse ints from env safely
-const parseIntEnv = (env: string | undefined, defaultValue: number) => {
-	if (env === undefined) {
-		return defaultValue;
-	}
-	const parsed = parseInt(env);
-	if (isNaN(parsed)) {
-		return defaultValue;
-	}
-	return parsed;
-};
 
 export const config: SceneDetectConfig = {
 	environment: process.env.NODE_ENV ?? 'development',
