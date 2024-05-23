b0c4560bf18d764d6bf71622d1311ea5dd37df20
Johan Bichel <public@johan.cc>
2024-05-23 07:33:02 +0200

    sh syntax

--- packages/loudness/install.sh
+++ packages/loudness/install.sh
@@ -16,9 +16,9 @@ cd loudness-scanner
 APT=$(which apt)
 APK=$(which apk)
 
-if [[ ! -z $APT ]]; then
+if [ ! -z $APT ]; then
   apt install -y git cmake libavformat-dev libavcodec-dev libavutil-dev libebur128-dev libsndfile1-dev 
-elif [[ ! -z $APK ]]; then
+elif [ ! -z $APK ]; then
   apk add --no-cache git cmake make g++ ffmpeg-libavformat ffmpeg-libavcodec ffmpeg-libavutil libebur128-dev libsndfile
 elif [ "$(uname)" = "Darwin" ]; then
   # install system dependencies on mac os with brew
