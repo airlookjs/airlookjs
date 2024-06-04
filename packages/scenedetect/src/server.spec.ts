830233b231b2e43c20c1ddd8422aa3dad77ba2df
Benjamin Malmgren-Hansen <bmalmh@gmail.com>
2024-06-04 12:41:56 +0200

    install deps github actions

--- packages/scenedetect/src/server.spec.ts
+++ packages/scenedetect/src/server.spec.ts
@@ -1,9 +1,11 @@
 import { server } from './server.js';
-import request from "supertest";
+import request from 'supertest';
+import express, { type Express } from "express";
+
 // import express, { type Express } from "express";
 import * as configExports from './config.js';
 
-import { expect, describe, it, vi, beforeEach, afterEach } from "vitest";
+import { expect, describe, it, vi, beforeEach, afterEach } from 'vitest';
 
 const TEST_FILE = 'test_file.mp4'; 
 
@@ -75,8 +77,8 @@ describe('scenedetect', () => {
 				  version: "1.0",
 				}
 			);
-    });
-  });
+		});
+	});
 
 	describe('shares that are cached', () => {
 		beforeEach(() => {
@@ -127,4 +129,62 @@ describe('scenedetect', () => {
 			);
     });
   });
+
+	describe('download file over url', ()  => {
+		let app: Express;
+		beforeEach(() => {
+			vi.spyOn(configExports, 'config', 'get').mockReturnValue({
+				...defaultConfig,
+				shares: [
+					{
+						name: 'no-mount-just-http',
+						mount: ``,
+						matches: [],
+						cached: false,
+						uncRoot: '',
+						systemRoot: '',
+					}
+				]
+			});
+
+			app = express();
+			// Setup express static middleware to look for files in the api directory for all requests starting with /api
+			app.use(`/tests`, express.static(`${import.meta.dirname}/../tests`) , function(_req, res){
+				// Optional 404 handler
+				res.status(404);
+				res.json({error:{code:404}})
+			});
+
+			app.listen('9090');
+		});
+
+		it('should return 200 OK', async () => {
+			const res = await request(server).get(`/api/scenedetect?file=http://127.0.0.1:9090/tests/${TEST_FILE}`);
+
+			expect(res.status).toBe(200);
+			expect(res.body).toEqual(
+				{
+				  scenedetect: {
+				    scenes: [
+				      {
+				        end: {
+				          frame: 901,
+									seconds: 30.033,
+				          timecode: "00:00:30.033",
+				        },
+				        index: 1,
+				        start: {
+				          frame: 1,
+				          seconds: 0,
+				          timecode: "00:00:00.000",
+				        },
+				      },
+				    ],
+				  },
+				  version: "1.0",
+				}
+			);
+		});
+	});
+
 });
